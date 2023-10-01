53e49f6d179eb10a9c3d52787c693757882a44c7
KarnamShyam1947 <karnamshyamgithub@gmail.com>
2023-10-02 01:12:43 +0530

    intial commit

--- Api/solution.py
+++ Api/solution.py
@@ -130,35 +130,174 @@ solution = {
     },
     
     10 : {
-        'class' : 'RISE HISPA',
+        'class' : 'RISE HEALTHY',
         'causes' : [
-
+            'It will check if the given crop is healthy or not if not it will suggest solutions'
         ],
         'solutions' : [
-
+            'It will check if the given crop is healthy or not if not it will suggest solutions'
         ]
 
     },
     
     11 : {
-        'class' : '',
+        'class' : 'RISE HISPA',
         'causes' : [
-
+            'Rice hispa is a type of leaf beetle that feeds on rice plants, causing damage to the leaves.',
+            'The larvae of rice hispa feed on the inner layers of rice leaves, leading to characteristic damage in the form of long, narrow white streaks or folds on the leaves.',
+            'Severe infestations of rice hispa can lead to reduced rice crop yields.'
         ],
         'solutions' : [
-
+            'Regularly monitor rice fields for signs of rice hispa infestations, such as white streaks on leaves or adult beetles.',
+            'Encourage natural predators like spiders, ladybugs, and parasitic wasps to help control rice hispa populations.',
+            'Implement crop rotation practices to reduce the buildup of rice hispa populations in the same fields.',
+            'Implement proper field management practices, such as maintaining proper water levels and avoiding excessive nitrogen fertilization, as these can affect rice hispa infestations.'
         ]
 
     },
     
     12 : {
-        'class' : '',
+        'class' : 'Rice leaf blast',
         'causes' : [
-
+            'The primary cause of rice leaf blast is the fungal pathogen Magnaporthe oryzae.',
+            'It thrives in warm and humid conditions, making rice plants most susceptible during the rainy season.',
+            'Some rice varieties are more susceptible to leaf blast than others, and younger plants are often more vulnerable.'
         ],
         'solutions' : [
-
+            'Plant rice varieties that are known to be resistant to leaf blast. Resistant varieties are available that can reduce the risk of infection.',
+            'Practice crop rotation to reduce the buildup of the fungus in the soil. Avoid planting rice in the same field consecutively.',
+            'Ensure proper plant spacing to promote air circulation and reduce humidity within the rice canopy.',
+            'In severe cases, fungicides can be used as a control measure. Consult with agricultural experts for guidance on the appropriate fungicides and application methods.',
         ]
-
     },
+
+    13 : {
+        'class' : 'Tomato bacterial spot',
+        'causes' : [
+            'The primary cause of tomato bacterial spot is the bacterium Xanthomonas campestris pv. vesicatoria.',
+            'The disease is favored by warm and humid weather conditions, which promote bacterial growth and infection.',
+            'Some tomato varieties are more susceptible to bacterial spot than others, and younger plants are often more vulnerable.'
+        ],
+        'solutions' : [
+            'Plant tomato varieties that are known to be resistant to bacterial spot. Resistant varieties can significantly reduce the risk of infection.',
+            'Practice crop rotation to reduce the buildup of the bacterium in the soil. Avoid planting tomatoes in the same area for consecutive seasons.',
+            'Maintain good garden hygiene by removing and destroying infected plant material promptly.',
+            'Use drip irrigation or soaker hoses to water tomato plants at the base, minimizing the wetting of foliage.'
+        ]
+    },
+
+    14 : {
+        'class' :  'Tomato early blight',
+        'causes' : [
+            'Tomato early blight is primarily caused by the fungus Alternaria solani.',
+            'The disease thrives in warm, humid, and wet conditions, making it more prevalent during rainy seasons.',
+            'The fungus can overwinter on infected plant debris, and spores can be spread by wind, rain, and splashing water.'
+        ],
+        'solutions' : [
+            'Practice crop rotation to reduce the buildup of the fungus in the soil. Avoid planting tomatoes or other related crops in the same area for consecutive seasons.',
+            'Remove and destroy infected plant material promptly to prevent the overwintering of the fungus.',
+            'Ensure proper plant spacing to promote air circulation and reduce humidity around tomato plants.',
+            'Apply mulch around the base of tomato plants to prevent soil splashing onto the lower leaves, which can spread the fungus.'
+        ]
+    }, 
+
+    15 : {
+        'class' : 'Tomato Healthy',
+        'causes' : [
+            'It will check if the given crop is healthy or not if not it will suggest solutions'
+        ],
+        'solutions' : [
+            'It will check if the given crop is healthy or not if not it will suggest solutions'
+        ]
+    },
+
+    16 : {
+        'class' : 'tomato late blight',
+        'causes' : [
+            'Late blight is caused by the water mold Phytophthora infestans.',
+            'It thrives in cool and wet conditions, particularly during periods of high humidity and rain.',
+            'The pathogen can spread through spore dissemination and direct contact with infected plant material.'
+        ],
+        'solutions' : [
+            'Use fungicides containing copper-based compounds or specific late blight fungicides preventatively, following recommended application guidelines.',
+            'Ensure adequate plant spacing to promote air circulation and reduce humidity around tomato plants.',
+            'Water at the base of plants using drip irrigation or soaker hoses to minimize foliage wetting.',
+            'Inspect new tomato plants for signs of late blight before introducing them to your garden.'
+        ]
+    },
+
+    17 : {
+        'class' : 'Tomato leaf mold',
+        'causes' : [
+            'Tomato leaf mold is primarily caused by the fungus Pseudoidium neolycopersici.',
+            'The disease thrives in warm and humid conditions, with temperatures around 70-75°F (21-24°C) and high humidity levels.',
+            'Tomato plants grown in greenhouses or indoor settings with limited ventilation are particularly susceptible to leaf mold.'
+        ],
+        'solutions' : [
+            'Maintain lower humidity levels in indoor growing environments by using proper ventilation, dehumidifiers, and ensuring adequate spacing between plants.',
+            'Choose tomato varieties that are known to be resistant to leaf mold if available in your region.',
+            'Remove and destroy infected plant material promptly to prevent the spread of the disease.',
+            'Apply fungicides preventatively if leaf mold has been a recurring issue in your area or if you are growing tomatoes in a high-humidity environment.'
+        ]
+    },
+
+    18 : {
+        'class' : 'Tomato Septoria leaf spot',
+        'causes' : [
+            'Tomato Septoria leaf spot is primarily caused by the fungus Septoria lycopersici.',
+            'The disease thrives in warm and humid conditions, particularly when there\'s high humidity and consistent moisture on the tomato leaves.',
+            'The fungus can overwinter in infected plant debris from the previous growing season, contributing to recurring infections.'
+        ],
+        'solutions' : [
+            'Avoid overhead watering to reduce moisture on the foliage. Use drip irrigation or soaker hoses to water at the base of the plants.',
+            'Apply mulch around the base of tomato plants to prevent soil splashing onto the lower leaves, which can carry fungal spores.',
+            'Choose tomato varieties that are known to be resistant to Septoria leaf spot if available in your region.',
+            'Apply fungicides and follow good cultural practices preventatively, especially if Septoria leaf spot has been a recurring problem in your garden.'
+        ]
+    },
+
+    19 : {
+        'class' : 'Tomato__Spider_mites_Two_spotted_spider_mite ',
+        'causes' : [
+            'Two-spotted spider mites are tiny arachnids that feed on the sap of tomato leaves, causing stippling and yellowing of the foliage.',
+            ' Hot and dry conditions can create favorable conditions for spider mite infestations, as they reproduce rapidly in warm weather.',
+            'Weakened or stressed tomato plants are more susceptible to spider mite attacks.'
+        ],
+        'solutions' : [
+            'Routinely inspect the undersides of tomato leaves for signs of spider mite infestations, such as tiny dots, stippling, or fine webbing.',
+            'Introduce natural predators of spider mites, such as ladybugs or predatory mites, to your garden to help control the pest population.',
+            'Implement good garden hygiene by removing plant debris and weeds, as these can harbor spider mites.',
+            'Ensure that your tomato plants are healthy and well-fertilized, as strong, vigorous plants are less attractive to spider mites.'
+        ]
+    },
+
+    20 : {
+        'class' : 'Tomato target spot',
+        'causes' : [
+            'Tomato target spot, or early blight, is caused by the fungus Alternaria solani.',
+            'The disease thrives in warm and humid conditions, particularly during periods of high humidity and rain.',
+            'The fungus can overwinter on infected plant debris, soil, and infected seeds, contributing to recurring infections.'
+        ],
+        'solutions' : [
+            'Remove and destroy infected plant material promptly, including leaves, stems, and fruit, to prevent the overwintering of the fungus.',
+            'Ensure proper plant spacing to promote air circulation and reduce humidity around tomato plants.',
+            'Apply mulch around the base of tomato plants to prevent soil splashing onto the lower leaves, which can carry fungal spores.',
+            ' Use disease-free seeds or treat seeds with a fungicide to prevent introducing the pathogen to your garden.'
+        ]
+    },
+
+    21 : {
+        'class' : 'Tomato Yellow Leaf Curl Virus',
+        'causes' : [
+            'TYLCV is caused by the transmission of the virus to tomato plants by the silverleaf whitefly (Bemisia tabaci).',
+            'The whitefly acts as a vector, feeding on infected plants and then transmitting the virus to healthy tomato plants as it feeds.'
+        ],
+        'solutions' : [
+            'Choose tomato varieties that are known to be resistant to TYLCV if available in your region. Resistant varieties can significantly reduce the risk of infection.',
+            'Use row covers or insect netting to physically exclude whiteflies from reaching your tomato plants.',
+            'Remove and destroy infected tomato plants and weeds in the vicinity, as they can serve as reservoirs for the virus.',
+            'Prune and remove infected plant material to prevent the spread of the virus to healthy plants.'
+        ]
+    }
+
 }
